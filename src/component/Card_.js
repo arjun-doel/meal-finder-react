941475d9a23dc418037ba3339e09bf92b333e9cd
Arjun Doel <arjundoel@gmail.com>
2021-06-19 19:04:48 +0100

    card start

--- src/component/Card_.js
+++ src/component/Card_.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
-const Card_ = () => {
+const Card_ = ({ name, image }) => {
   return (
     <>
-      <div>
-        Card
+      <div className="meal-card">
+        <img src={image} alt={name} />
+        <div className="meal-card-body">
+          <h5>{name}</h5>  
+        </div>
       </div>
     </>
   )
--- src/component/main/Home.js
+++ src/component/main/Home.js
@@ -1,21 +1,24 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import SearchBar from './SearchBar'
+import Card_ from '../Card_'
 
 const Home = () => {
   const [userInput, setUserInput] = useState('')
+  const [meals, setMeals] = useState([])
 
   useEffect(() => {
     const getData = async () => {
       try {
-        const response = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${userInput}`)
-        console.log(response)
+        const { data } = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${userInput}`)
+        setMeals(data.meals)
       } catch (err) {
         console.log(err)
       }
     }
     getData()
   }, [userInput])
+  console.log(meals)
 
 
 
@@ -33,7 +36,18 @@ const Home = () => {
           getUserInput={getUserInput}
         />
         <div className="wrapper">
-          <h1>Home</h1>
+          {!meals ? <p>Sorry this recipe does not exist</p> :
+            meals.map(meal =>
+              <>
+                <Card_
+                  key={meal.idMeal}
+                  name={meal.strMeal}
+                  image={meal.strMealThumb}
+                />
+              </>
+            )
+
+          }
         </div>
       </section>
     </>
