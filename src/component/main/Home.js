b9d51445ca2839c0e220e8a69eccd0061c504b3a
Arjun Doel <arjundoel@gmail.com>
2021-06-19 13:09:41 +0100

    search bar added

--- src/component/main/Home.js
+++ src/component/main/Home.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import SearchBar from './SearchBar'
 
 const Home = () => {
   return (
     <>
+      <section className="home">
+        <SearchBar />
+        <div className="wrapper">  
+          <h1>Home</h1>
+        </div>
+      </section>
     </>
   )
 }
