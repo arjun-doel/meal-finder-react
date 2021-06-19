443c453ea715b2375f6854c1bdb27b17968e3f46
Arjun Doel <arjundoel@gmail.com>
2021-06-19 09:34:41 +0100

    readme updated & navbar sass complete

--- src/component/main/Navigation.js
+++ src/component/main/Navigation.js
@@ -3,7 +3,20 @@ import React from 'react'
 const Navigation = () => {
   return (
     <>
-      <h1>Nav</h1>
+      <nav>
+        <header>
+          <div className="name">
+            <h1>Meal Finder</h1>
+            <div className="drum-stick">
+              <i className="fas fa-hamburger"></i>
+            </div>
+          </div>
+          
+          <div className="heart">
+            <i className="fas fa-heart"></i>
+          </div>
+        </header>
+      </nav>
     </>
   )
 }
